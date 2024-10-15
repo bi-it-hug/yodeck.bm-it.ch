73c078663f00b0c6f66214e112bec34eb37a0cb4
Lorenzo Hug <lorenzo.hug@espas.ch>
2024-10-15 16:47:55 +0200

    aaaaaaaaaaaahhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhh

--- scripts/getUser.js
+++ scripts/getUser.js
@@ -1,3 +1,5 @@
+const tvBox = document.getElementById('tvBox')
+const noData = document.querySelector('.no-data')
 const imgElement = document.getElementById('userImage')
 const usernameElement = document.getElementById('username')
 
@@ -21,24 +23,35 @@ async function getData(url) {
     try {
         const resp = await fetch(url, fetchOptions)
         if (!resp.ok) throw new Error(`HTTP error! status: ${resp.status}`)
-
         return await resp.json()
+
     } catch (error) {
         console.error('Error fetching data:', error)
     }
 }
 
-async function loadData(url) {
-    try {
-        const data = await getData(url)
 
-        if (data && data.tasks.length > 0 && data.tasks[0].assignees.length > 0) {
-            const assignee = data.tasks[0].assignees[0]
-            imgElement.src = assignee.profilePicture
-            usernameElement.textContent = assignee.username
+async function loadData(url) {
+    if (apiKey) {
+        tvBox.classList.add('show')
+
+        try {
+            const data = await getData(url)
+
+            if (data && data.tasks.length > 0 && data.tasks[0].assignees.length > 0) {
+                const assignee = data.tasks[0].assignees[0]
+
+                imgElement.src = assignee.profilePicture || 'default-profile.png'
+                usernameElement.textContent = assignee.username || 'Unknown User'
+
+            } else {
+                noData.classList.add('show')
+            }
+        } catch (error) {
+            noData.classList.add('show')
         }
-    } catch (error) {
-        console.error('Error loading profile picture:', error)
+    } else {
+        noData.classList.add('show')
     }
 }
 
