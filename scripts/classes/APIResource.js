8ffde3061826715811d7fe3046dc9cf398080ab2
Lorenzo Hug <lorenzo.hug@espas.ch>
2026-01-07 14:37:03 +0100

    Refactor code structure for improved readability and maintainability

--- scripts/classes/APIResource.js
+++ scripts/classes/APIResource.js
@@ -1,20 +1,19 @@
 class APIResource {
     constructor(id, type) {
-        this.id = id
-        this.type = type
+        this.id = id;
+        this.type = type;
     }
 
     get URL() {
-
         switch (this.type) {
-            case 'task':
-                return `https://api.clickup.com/api/v2/task/${this.id}`
+            case "task":
+                return `https://api.clickup.com/api/v2/task/${this.id}`;
 
-            case 'task-list':
-                return `https://api.clickup.com/api/v2/list/${this.id}/task`
+            case "task-list":
+                return `https://api.clickup.com/api/v2/list/${this.id}/task`;
 
             default:
-                return `https://api.clickup.com/api/v2/${this.type}/${this.id}`
+                return `https://api.clickup.com/api/v2/${this.type}/${this.id}`;
         }
     }
 }
--- scripts/main/data.js
+++ scripts/main/data.js
@@ -1,76 +1,70 @@
-const error = document.getElementById('error')
-const username = document.getElementById('username')
-const portrait = document.getElementById('portrait')
-const assigneeBox = document.getElementById('assignee-box')
-const loadingBox = document.getElementById('loading-box')
+const error = document.getElementById("error");
+const username = document.getElementById("username");
+const portrait = document.getElementById("portrait");
+const assigneeBox = document.getElementById("assignee-box");
+const loadingBox = document.getElementById("loading-box");
 
-const apiKey = new URLSearchParams(window.location.search).get('key')
+const apiKey = new URLSearchParams(window.location.search).get("key");
 
 const resources = {
-    assigneeTask: new APIResource('8698apucx', 'task'),
-    assigneeTaskList: new APIResource('901202079960', 'task-list')
-}
+    assigneeTask: new APIResource("8698apucx", "task"),
+    assigneeTaskList: new APIResource("901202079960", "task-list"),
+};
 
 const data = {
     get: async function (URL) {
         try {
             const response = await fetch(URL, {
-                method: 'GET',
+                method: "GET",
                 headers: {
-                    accept: 'application/json',
-                    Authorization: apiKey
-                }
-            })
-            if (!response.ok) console.error(response)
-            return await response.json()
-
+                    accept: "application/json",
+                    Authorization: apiKey,
+                },
+            });
+            if (!response.ok) console.error(response);
+            return await response.json();
         } catch (error) {
-            console.error(`Error: Failed to fetch data: ${error}`)
+            console.error(`Error: Failed to fetch data: ${error}`);
         }
     },
 
     render: async function (RESOURCE) {
-
-        console.info('Loading data...')
+        console.info("Loading data...");
 
         if (apiKey) {
             try {
-                const data = await this.get(RESOURCE.URL)
-                let assignee
+                const data = await this.get(RESOURCE.URL);
+                let assignee;
 
                 switch (RESOURCE) {
                     case resources.assigneeTaskList:
-
-                        assignee = data.tasks[0].assignees[0]
+                        assignee = data.tasks[0].assignees[0];
 
                         if (assignee.profilePicture === null) {
-                            portrait.src = '../assets/images/not-found.svg'
-
+                            portrait.src = "../assets/images/not-found.svg";
                         } else {
-                            portrait.src = assignee.profilePicture
+                            portrait.src = assignee.profilePicture;
                         }
 
                         if (cutAbbreviation) {
-                            username.textContent = assignee.username.replace(/\s*\(.*?\)/, '')
-
+                            username.textContent = assignee.username.replace(/\s*\(.*?\)/, "");
                         } else {
-                            username.textContent = assignee.username
+                            username.textContent = assignee.username;
                         }
 
-                        assigneeBox.classList.add('show-assignee')
-                        break
+                        assigneeBox.classList.add("show-assignee");
+                        break;
                 }
-
             } catch (error) {
-                console.error(`Error: Failed to load data: ${error}`)
-                error.classList.add('show-error')
+                console.error(`Error: Failed to load data: ${error}`);
+                error.classList.add("show-error");
             }
         } else {
-            console.error('Error: No key given')
-            error.classList.add('show-error')
+            console.error("Error: No key given");
+            error.classList.add("show-error");
         }
-        loadingBox.classList.remove('show-loading')
-    }
-}
+        loadingBox.classList.remove("show-loading");
+    },
+};
 
-window.onload = () => data.render(resources.assigneeTaskList)
+window.onload = () => data.render(resources.assigneeTaskList);
